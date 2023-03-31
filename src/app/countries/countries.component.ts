6df7630c8f9d2136fb003df5cc2b29f007888767
Michal Rewers <mrewers95@wp.pl>
2023-03-31 15:38:47 +0200

    Fix class, add new files

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,9 +1,9 @@
-import { Component, ViewChild, Inject, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
-import { MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import * as Leaflet from 'leaflet';
+import { MatDialog } from '@angular/material/dialog';
+import { MapLocationDialog } from './location-dialog.component';
 
 @Component({
   selector: 'app-countries',
@@ -51,46 +51,5 @@ export class CountriesComponent {
   }
   
 }
-
-@Component({
-  selector: 'location-dialog',
-  templateUrl: 'location-dialog.html',
-  styleUrls: ['location-dialog.component.css']
-})
-export class MapLocationDialog implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient) {}
-
-  countryName = this.data.element.name;
-  url = `http://api.worldbank.org/v2/country/${this.data.element.iso2Code}?format=json`;
-  lat = this.data.element.latitude;
-  lng = this.data.element.longitude;
-  options = {
-    layers: getLayers(),
-    zoom: 10,
-    center: new Leaflet.LatLng(this.lat, this.lng)
-  };
-  
-  getLocationInfo() {
-    this.http.get<any>(this.url).subscribe(
-      (result) => {
-        this.lat = result[1][0].latitude;
-        this.lng = result[1][0].longitude;
-      }
-    );
-  }
-  
-  ngOnInit() {
-    this.getLocationInfo();
-  }
-}
-
-export const getLayers = (): Leaflet.Layer[] => {
-  return [
-    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    } as Leaflet.TileLayerOptions),
-  ] as Leaflet.Layer[];
-};
 
 
